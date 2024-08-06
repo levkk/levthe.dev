84ef9067229c6cbf657ba75afc15ed638a0b4334
Lev Kokotov <lev.kokotov@gmail.com>
2024-08-06 11:22:32 -0700

    save

--- content/posts/language-interpreter-from-scratch-part-1/interpreter/src/main.rs
+++ content/posts/language-interpreter-from-scratch-part-1/interpreter/src/main.rs
@@ -1,14 +1,16 @@
 #![allow(unused_mut, dead_code, unused_variables)]
-use std::iter::{Peekable, Iterator};
+use std::iter::Iterator;
+use std::ops::{Add, Mul};
 
 /// List of all available tokens in our language.
 #[derive(Debug)]
 pub enum Token {
     Number(i64),
     Plus,
+    String(String),
+    Star,
 }
 
-/// Lexer takes a string and returns a list of tokens.
 pub struct Lexer<'a> {
     // Source code.
     source: &'a str,
@@ -19,7 +21,6 @@ pub struct Lexer<'a> {
 }
 
 impl<'a> Lexer<'a> {
-    /// Create new lexer for the source code.
     pub fn new(source: &'a str) -> Lexer {
         Lexer {
             source,
@@ -28,129 +29,217 @@ impl<'a> Lexer<'a> {
         }
     }
 
-    /// Convert code into a list of tokens, consuming the lexer.
-   pub fn tokens(&mut self) -> Vec<Token> {
-        // Extract tokens one character at a time.
-        use Token::*;
+    /// Extract tokens one character at a time.
+    pub fn tokens(&mut self) -> Vec<Token> {
+        let mut chars = self.source.chars();
     
-        for c in self.source.chars() {
+        while let Some(c) = chars.next() {
             match c {
-                // Spaces separate tokens.
                 ' ' => self.process_token(),
                 '0'..='9' => self.buffer.push(c),
-                '+' => self.tokens.push(Plus),
+                '+' => self.tokens.push(Token::Plus),
+    
+                // Double quote indicating the start of a string.
+                '"' => {
+                    let mut string = String::new();
+    
+                    while let Some(c) = chars.next() {
+                        match c {
+                            // Closing double quote ends the string.
+                            '"' => break,
+                            _ => string.push(c),
+                        }
+                    }
+    
+                    self.tokens.push(Token::String(string));
+                },
+                '*' => self.tokens.push(Token::Star),
+    
                 c => todo!("lexer error, unsupported character: '{}'", c),
             }
         }
-        
-        // Don't forget to process whatever is in the buffer
-        // at the end of the input:
+    
         self.process_token();
     
         std::mem::take(&mut self.tokens)
     }
-    
-    /// Process a multi-character token stored in the buffer.
-    fn process_token(&mut self) {
-        use Token::*;
 
-        // Empty buffer means no more tokens in the input.
+    fn process_token(&mut self) {
+        // Empty buffer means there is nothing to do here.
         if self.buffer.is_empty() {
             return;
         }
 
-        // Use the standard library str::parse
+        // Use the standard library's [`str::parse`]
         // to convert text to an integer.
-        self.tokens.push(
-            Number(self.buffer.as_str().parse().unwrap())
-        );
+        self.tokens
+            .push(Token::Number(self.buffer.as_str().parse().unwrap()));
 
         // Clear the buffer for the next token.
         self.buffer.clear();
     }
 }
 
+/// An operation. Only addition currently supported.
 #[derive(Debug)]
 enum Operation {
-    /// Addition operation.
     Addition,
+    Multiplication,
 }
 
-#[derive(Debug)]
+/// A constant value. Currently, only numbers are supported.
+#[derive(Debug, Clone)]
 enum Value {
-    /// A value can be a number. Our language translates
-    /// all numbers to 64-bit signed integers, which makes things easier,
-    /// but we can add more types later.
     Number(i64),
+    /// A value storing a string.
+    String(String),
 }
 
+impl Add for Value {
+    type Output = Value;
+
+    fn add(self, other: Value) -> Value {
+        match (self, other) {
+            (Value::Number(a), Value::Number(b)) => Value::Number(a + b),
+
+            // Supports 21 + "hello world"
+            (
+                Value::Number(a),
+                Value::String(s),
+            ) => Value::String(a.to_string() + &s),
+
+            // Supports "hello world" + 21
+            (
+                Value::String(s),
+                Value::Number(a),
+            ) => Value::String(s + a.to_string().as_str()),
+
+            (a, b) => todo!(
+                "syntax error, '+' between {:?} and {:?} not supported",
+                a, b
+            ),
+        }
+    }
+}
+
+impl Mul for Value {
+    type Output = Value;
+
+    fn mul(self, other: Value) -> Value {
+        match (self, other) {
+            (Value::Number(a), Value::Number(b)) => Value::Number(a * b),
+
+            // Supports 21 + "hello world"
+            (
+                Value::Number(a),
+                Value::String(s),
+            ) => Value::String(s.repeat(a as usize)),
+
+            // Supports "hello world" + 21
+            (
+                Value::String(s),
+                Value::Number(a),
+            ) => Value::String(s.repeat(a as usize)),
+
+            (a, b) => todo!(
+                "syntax error, '+' between {:?} and {:?} not supported",
+                a, b
+            ),
+        }
+    }
+}
+
+/// Expression term. Currently, only values are supported.
 #[derive(Debug)]
 enum Term {
-    /// A term is just a constant value.
     Value(Value),
 }
 
 #[derive(Debug)]
 enum Expression {
-    /// A single term.
-    Term(Term),
-
     /// A binary operation.
-    BinaryOp {
-        left: Box<Expression>,
+    Binary {
+        left: Term,
         op: Operation,
-        right: Box<Expression>,
+        right: Term,
     },
+
+    /// Just a term by itself.
+    Term(Term),
 }
 
 impl Expression {
     /// Given a stream of tokens, parse a single expression.
-    pub fn parse(
-        stream: &mut Peekable<impl Iterator<Item = Token>>
-    ) -> Expression {
+    pub fn parse(stream: &mut impl Iterator<Item = Token>) -> Expression {
         let left = Self::term(stream);
-        let op = stream.next();
+        let operation = stream.next();
 
-        match op {
-            Some(op) => {
-                let op = match op {
+        match operation {
+            Some(operation) => {
+                let op = match operation {
                     Token::Plus => Operation::Addition,
-                    _ => panic!("syntax error, expected operator, got: {:?}", op),
+                    Token::Star => Operation::Multiplication,
+                    _ => panic!("syntax error, expected operation, got: {:?}", operation),
                 };
 
-                let right = Expression::term(stream);
-                Expression::BinaryOp {
-                    left: Box::new(left),
-                    op,
-                    right: Box::new(right),
-                }
+                let right = Self::term(stream);
+
+                Expression::Binary { left, op, right }
             }
 
-            None => left,
+            None => Expression::Term(left),
         }
     }
 
     /// Given a stream of tokens, parse a single term.
-    fn term(
-        stream: &mut Peekable<impl Iterator<Item = Token>>
-    ) -> Expression {
-        let token = stream.next().expect("parse eof");
+    fn term(stream: &mut impl Iterator<Item = Token>) -> Term {
+        let token = stream.next().expect("expected a token");
 
         match token {
-            Token::Number(n) => {
-                Expression::Term(Term::Value(Value::Number(n)))
-            }
-
+            Token::Number(n) => Term::Value(Value::Number(n)),
+            Token::String(s) => Term::Value(Value::String(s)),
             _ => panic!("syntax error, expected term, got: {:?}", token),
+        }
+    }
+    
+    pub fn evaluate(&self) -> Value {
+        match self {
+            Expression::Term(Term::Value(value)) => value.clone(),
+            Expression::Binary {
+                left: Term::Value(left),
+                op,
+                right: Term::Value(right)
+            } => {
+                match op {
+                    Operation::Addition => {
+                        left.clone() + right.clone()
+                    }
+                    
+                    Operation::Multiplication => {
+                        left.clone() * right.clone()
+                    }
+                }
+            },
         }
     }
 }
 
 fn main () {
-    let source = "21 + 2";
+    eval(r#""hello" * 3"#);
+}
+
+fn eval(source: &str) {
+    // Parse the code into tokens.
     let mut lexer = Lexer::new(source);
-    println!("{:?}", lexer.tokens());
-    println!("{:?}", Expression::parse(
-        &mut lexer.tokens().into_iter().peekable()
-    ));
+    let tokens = lexer.tokens();
+
+    // Parse the tokens into an AST.
+    let expression = Expression::parse(&mut tokens.into_iter());
+
+    println!("{:#?}", expression);
+
+    // Execute the AST producing a single value.
+    let result = expression.evaluate();
+
+    println!("{:?}", result);
 }
